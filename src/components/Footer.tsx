2c7d52c2fae611f0b6784527dd208daebf9f42de
duckb3ar <160338513+bluDuckB3ar@users.noreply.github.com>
2024-08-25 19:50:36 -0400

    modified:   src/components/Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ const Footer = () => {
 
     return (
         <div className="mt-4">
+            
             <a href="https://twitter.com/Twitter" className="text-duckBlue2 mx-2">Twitter</a>
             <a href="https://www.linkedin.com" className="text-duckBlue2 mx-2">LinkedIn</a>
             <a href="https://github.com" className="text-duckBlue2 mx-2">GitHub</a>
