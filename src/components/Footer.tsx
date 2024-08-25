4dcb2b7b31324e0dfdfa28083ff323524a59adc9
duckb3ar <160338513+bluDuckB3ar@users.noreply.github.com>
2024-08-25 19:25:37 -0400

    modified:   src/components/Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,8 @@ const Footer = () => {
             <a href="https://twitter.com/Twitter" className="text-duckBlue2 mx-2">Twitter</a>
             <a href="https://www.linkedin.com" className="text-duckBlue2 mx-2">LinkedIn</a>
             <a href="https://github.com" className="text-duckBlue2 mx-2">GitHub</a>
+            <a href='https://www.instagram.com' className="text-duckBlue2 mx-2">Instagram</a>
+            <a href='https://www.Mastodon.social' className="text-duckBlue2 mx-2">Mastodon</a>
           </div>
     );
 };
