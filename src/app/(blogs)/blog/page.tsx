8cc59d4e142ae7b1c47dba99c730e30ad6939dfa
duckb3ar <160338513+bluDuckB3ar@users.noreply.github.com>
2024-12-14 03:35:12 -0500

    Update BlogPage styles and upgrade dependencies in package.json

--- src/app/(blogs)/blog/page.tsx
+++ src/app/(blogs)/blog/page.tsx
@@ -72,7 +72,7 @@ export default async function BlogPage() {
   ];
 
   return (
-    <div className="container max-w-4xl py-6 lg:py-10">
+    <div className="container max-w-4xl py-6 text-bold  lg:py-10">
       <div className="flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <div className="flex overflow-hidden text-doto space-x-8 group">
