3a323f24cbcbacdb27b5e06b2e189d7cc096382d
duckb3ar <160338513+bluDuckB3ar@users.noreply.github.com>
2025-01-23 04:32:43 -0500

    modified:   src/app/(blogs)/blog/[slug]/page.tsx

--- src/app/(blogs)/blog/[slug]/page.tsx
+++ src/app/(blogs)/blog/[slug]/page.tsx
@@ -1,37 +1,37 @@
+import { Mdx } from "@/components/mdx/mdx-comp";
 import { allPages } from "contentlayer/generated"
-import React from "react"
-import { Mdx } from "@/components/mdx/mdx-comp"
 import { notFound } from "next/navigation"
 
+// Create a map for faster lookups
+const pagesMap = new Map(allPages.map(page => [page.slugAsParams, page]));
 
+/**
+ * Retrieves a document based on the provided slug.
+ * @param {string} slug - The slug of the document to retrieve.
+ * @returns {Promise<object>} The document object if found, otherwise calls notFound.
+ */
 async function getDocFromParams(slug: string) {
-    const doc = allPages.find((doc) => doc.slugAsParams === slug)
-    if (!doc)
-        notFound()
-    return doc
-
+    const doc = pagesMap.get(slug);
+    if (!doc) return notFound();
+    return doc;
 }
 
 interface PageProps {
     params: {
-        slug: string
-    }
+        slug: string;
+    };
 }
 
-const page = async ({ params }: PageProps) => {
-    const doc = await getDocFromParams(params.slug)
-    doc.title
-    doc.description
-
+const Page = async ({ params }: PageProps) => {
+    const doc = await getDocFromParams(params.slug);
 
     return (
         <div>
-            <h1>    doc.title     </h1>
+            <h1>{doc.title}</h1>
             <Mdx code={doc.body.code} />
-
+            <h1>{doc.title}</h1>
         </div>
-    )
-}
+    );
+};
 
-
-export default page 
+export default Page;
